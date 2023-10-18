128a68e1205370654e4fbcb4c908b0f48cbf9ef6
Harman Mann <76751076+HarmanSMann@users.noreply.github.com>
2023-10-18 13:02:17 -0400

    Update W3School.kt
    basics have now been covered

--- Kotlin/Notes/W3School.kt
+++ Kotlin/Notes/W3School.kt
@@ -14,6 +14,7 @@
     //2
         val birthyear = 1975
     //3
+print
         var carName = "Volvo"
         println(carName)
     //4
@@ -37,3 +38,62 @@
         val y = 3
         println(x == y)
 
+//5 - Strings
+        var carName = "Volvo"
+        println(txt1.compareTo(txt2))
+
+// 6 - Boolean
+        val x = 10
+        val y = 9
+        println(x > y)
+
+//7 - if...Else
+
+val time = 20
+val greeting = if (time <18 ) {
+    "Good day"
+    } else {
+    "Good Evening"
+}
+println(greeting)       
+
+
+//8 - While
+var i = 0
+while (i < 10) {
+  if (i == 4) {
+    i++
+    continue //skip
+    //break leave loop
+  }
+  println(i)
+  i++
+}
+
+//9 - Arrays
+val cars = arrayOf("Volvo", "BMW", "Ford", "Mazda")
+//same as java, [#], .size
+
+// 10 - For
+for (x in cars) //do something
+for (nums in 5..15) //so something
+
+//11 - Functions
+
+fun myFunction(fname: String) {
+  println(fname + " Doe")
+}
+
+fun main() {
+  myFunction("John")
+}
+
+//11 - Classes
+class Car(var brand: String, var model: String, var year: Int)
+
+fun main() {
+  val c1 = Car("Ford", "Mustang", 1969)
+  println(c1.brand)
+}
+
+open class MyClass
