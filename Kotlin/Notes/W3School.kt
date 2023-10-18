00034a3ac95dde1805986b7816abeaead57e9624
Harman Mann <76751076+HarmanSMann@users.noreply.github.com>
2023-10-18 12:46:40 -0400

    Update W3School.kt
    Variables and Data Types

--- Kotlin/Notes/W3School.kt
+++ Kotlin/Notes/W3School.kt
@@ -8,3 +8,20 @@
     //3
         /* multi-line comment*/
 
+// 2 - Variables
+    //1
+        var carName = "Volvo"
+    //2
+        val birthyear = 1975
+    //3
+        var carName = "Volvo"
+        println(carName)
+    //4
+        val x = 5 //non mutable -> val
+        val y = 10
+        println(x + y)
+
+//3 - Data Types
+    // -> Int, String, Boolean
+
+
